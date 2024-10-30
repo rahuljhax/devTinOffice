221fad2cfc90a3fe6a5681bdeae7cad1a890e39e
therahuljhacom <rahuljha.digilantern@gmail.com>
2024-10-30 14:43:23 +0530

    exploring the routes

--- src/app.js
+++ src/app.js
@@ -1,15 +1,20 @@
 const express = require('express');
 const app = express();
 
-
-app.use('/', (req, res) => {
-    res.send('Hello I am your apis')
-})
-app.use('/user', (req, res) => {
-    res.send('Hello I am your user')
+app.get('/user', (req, res) => {
+    res.send({ firstname: 'Rahul Jha', age: 23 })
 })
 
+app.post('/user', (req, res) => {
+    console.log('Data Successfully sumbitted on Database!')
+    res.send('Data saved successfully!')
+})
+
+app.delete('/user', (req, res) => {
+    console.log('User deleted successfully!')
+    res.send('User deleted successfully!')
+})
 
 app.listen(3000, () => {
-    console.log('Your app is running on PORT 3000..')
+    console.log('Server is running on 3000 PORT...')
 })
